9f5eeeeef4c33dacf74535a253008e778cde4ea8
HENRIQUE DE ROSSI LEITAO <00001107787038sp@aluno.educacao.sp.gov.br>
2024-06-04 13:54:58 -0300

    quase finalizando

---
+++ is.js
@@ -0,0 +1,6 @@
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fIMC = fPeso/(fAltura*fAltura)
+    
+}
